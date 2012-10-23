0dc38e92f831b4e90493ef08ce1e62a76c094aca
th4 <th4@th4-K53E.(none)>
2012-10-23 23:57:48 +0900

    収束してない

--- stochastic_gradient_descent_method101.R
+++ stochastic_gradient_descent_method101.R
@@ -30,43 +30,41 @@ w_conv_list=list(numeric,numeric,numeric,numeric,numeric)#wが収束した時の
 for(cnt in 1:N){
     #初期値
     x=rnorm(5)#N(0,1)の乱数
-    for(i in 1:5){#?
-        
-        y_list[[i]]=w_list[[i]]%*%x
-        w_previous_list[[i]]=w_list[[i]]
-        orthogonal=numeric(5)
-        
-        for(k in 1:5){
-            if(end_list[[k]]==1){#収束した時(end_list[[k]]==1)収束したものを使う
-                w_list[[k]]=w_conv_list[[k]]
-                if(i>=2){
-                    y_list[[j]]=w_list[[j]]%*%x
-                    for(j in 1:(i-1)){#
-                        cat("j:",j,"\n")
-                        orthogonal=orthogonal+y_list[[j]]*w_list[[j]]
+    for(i in 1:5){
+        if(end_list[[i]]==0){#収束していないwについて処理する
+            y_list[[i]]=w_list[[i]]%*%x
+            w_previous_list[[i]]=w_list[[i]]
+            orthogonal=numeric(5)
+            if(i>=2){
+                for(k in 1:5){
+                    if(end_list[[k]]==1){#収束した時(end_list[[k]]==1)収束したものを使う
+                        w_list[[k]]=w_conv_list[[k]]
+                        
+                            for(j in 1:(i-1)){#
+                                y_list[[j]]=w_list[[j]]%*%x
+                                orthogonal=orthogonal+y_list[[j]]*w_list[[j]]
+                            }
                     }
-                }
+                }    
             }
-        }    
-        
-        w_list[[i]]=w_previous_list[[i]]+r*y_list[[i]]*(x-y_list[[i]]*w_previous_list[[i]]-2*orthogonal)
-        
-        cat("cnt==",cnt,":\n")
-        cat(w_list[[i]],"\n")
-        cat(w_previous_list[[i]],"\n")
-        if((w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]])<epsilon){
-            w_conv_list[[i]]=w_list[[i]]
-            end_list[[i]]=1
-            cat("(w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]])<epsilon\n")
-            cat("収束",(w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]]),"\n\n")
+            w_list[[i]]=w_previous_list[[i]]+r*y_list[[i]]*(x-y_list[[i]]*w_previous_list[[i]]-2*orthogonal)
+            cat("i==",i,"::")
+            cat("cnt==",cnt,":\n")
             cat(w_list[[i]],"\n")
-            cat(w_previous_list[[i]])
-        }else if(cnt==N){
-            cat("cnt==",N,"\n")
-            cat(w_list[[i]],"\n")
-            cat(w_previous_list[[i]])
+            cat(w_previous_list[[i]],"\n\n")
+            if((w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]])<epsilon){
+                w_conv_list[[i]]=w_list[[i]]
+                end_list[[i]]=1
+                cat("(w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]])<epsilon\n")
+                cat("収束",(w_list[[i]]-w_previous_list[[i]])%*%(w_list[[i]]-w_previous_list[[i]]),"\n\n")
+                cat(w_list[[i]],"\n")
+                cat(w_previous_list[[i]],"\n\n")
+            }else if(cnt==N){
+                cat("cnt==",N,"\n")
+                cat(w_list[[i]],"\n")
+                cat(w_previous_list[[i]])
+            }
         }
-    
     }
     
 }
